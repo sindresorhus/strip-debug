32410e6ebcacd0434120517714ad2338f34ad9ea
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-22 02:02:44 +0100

    update tests

--- test.js
+++ test.js
@@ -3,14 +3,14 @@ var assert = require('assert');
 var stripDebug = require('./index');
 
 it('should strip debugger statement', function () {
-	assert.equal(stripDebug('function test(){debugger;}'), 'function test(){}');
-	assert.equal(stripDebug('"use strict";debugger;foo()'), '"use strict";foo()');
+	assert.equal(stripDebug('function test(){debugger;}').toString(), 'function test(){}');
+	assert.equal(stripDebug('"use strict";debugger;foo()').toString(), '"use strict";foo()');
 });
 
 it('should strip console statement', function () {
-	assert.equal(stripDebug('function test(){console.log();}'), 'function test(){}');
-	assert.equal(stripDebug('function test(){window.console.log();}'), 'function test(){}');
-	assert.equal(stripDebug('function test(){global.console.log();}'), 'function test(){}');
-	assert.equal(stripDebug('"use strict";console.log("foo");foo()'), '"use strict";foo()');
-	assert.equal(stripDebug('if(console){global.console.log("foo", "bar");}'), 'if(console){}');
+	assert.equal(stripDebug('function test(){console.log();}').toString(), 'function test(){}');
+	assert.equal(stripDebug('function test(){window.console.log();}').toString(), 'function test(){}');
+	assert.equal(stripDebug('function test(){global.console.log();}').toString(), 'function test(){}');
+	assert.equal(stripDebug('"use strict";console.log("foo");foo()').toString(), '"use strict";foo()');
+	assert.equal(stripDebug('if(console){global.console.log("foo", "bar");}').toString(), 'if(console){}');
 });
