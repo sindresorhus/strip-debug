647b8d1153f1abe17ab3b4c14313dfc448922d93
Sindre Sorhus <sindresorhus@gmail.com>
2015-03-12 14:00:01 +0800

    prevent memory leak
    fixes #9
    fixes sindresorhus/gulp-strip-debug#7

--- test.js
+++ test.js
@@ -31,3 +31,9 @@ it('should never strip away non-debugging code', function () {
 	var t = 'var test = {\n    getReadSections: function(){\n        var readSections = window.localStorage.getItem(\'storyReadSections\') || \'[]\';\n        return JSON.parse(readSections);\n    }\n};';
 	assert.equal(stripDebug(t).toString(), t);
 });
+
+it('shouldn\'t leak memory', function () {
+	assert.doesNotThrow(function () {
+		stripDebug('var obj = null; try { obj = \'something\'; } catch (e) { console.warn(\'NOPE!\'); }').toString();
+	});
+});
