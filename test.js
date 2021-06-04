98b0abfd54da223d43f291de8cd7e8b271473b6d
Tiger Oakes <contact@tigeroakes.com>
2021-06-03 14:27:12 -1000

    Add globalThis, console.*

--- test.js
+++ test.js
@@ -17,7 +17,9 @@ test('strip debugger statement', async t => {
 
 test('strip console statement', async t => {
 	t.is(await stripDebug('function test(){console.log("foo");}'), 'function test() {\n  void 0;\n}');
+	t.is(await stripDebug('function test(){console.warn("foo");}'), 'function test() {\n  void 0;\n}');
 	t.is(await stripDebug('function test(){window.console.log("foo");}'), 'function test() {\n  void 0;\n}');
+	t.is(await stripDebug('function test(){globalThis.console.log("foo");}'), 'function test() {\n  void 0;\n}');
 	t.is(await stripDebug('var test = () => console.log("foo");'), 'var test = () => void 0;');
 	t.is(await stripDebug('"use strict";console.log("foo");foo()'), '"use strict";\n\nvoid 0;\nfoo();');
 	t.is(await stripDebug('if(console){console.log("foo", "bar");}'), 'if (console) {\n  void 0;\n}');
@@ -29,6 +31,7 @@ test('strip console statement', async t => {
 test('strip alert statement', async t => {
 	t.is(await stripDebug('function test(){alert("foo");}'), 'function test() {\n  void 0;\n}');
 	t.is(await stripDebug('function test(){window.alert("foo");}'), 'function test() {\n  void 0;\n}');
+	t.is(await stripDebug('function test(){globalThis.alert("foo");}'), 'function test() {\n  void 0;\n}');
 	t.is(await stripDebug('var test = () => alert("foo");'), 'var test = () => void 0;');
 	t.is(await stripDebug('"use strict";alert("foo");foo()'), '"use strict";\n\nvoid 0;\nfoo();');
 	t.is(await stripDebug('if(alert){alert("foo", "bar");}'), 'if (alert) {\n  void 0;\n}');
@@ -39,7 +42,9 @@ test('strip alert statement', async t => {
 test('never strip away non-debugging code', async t => {
 	const fixture = `var test = {
   getReadSections: function () {
+    foo.console.log('started');
     var readSections = window.localStorage.getItestripDebug('storyReadSections') || '[]';
+    foo.alert('done');
     return JSON.parse(readSections);
   }
 };`;
