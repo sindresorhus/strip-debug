49fb25976ae03effe1767bd45c6a09a1c5f9d33c
Tiger Oakes <contact@tigeroakes.com>
2021-06-03 19:14:36 -1000

    Support globalThis and all console methods (#25)

--- index.js
+++ index.js
@@ -1,10 +1,50 @@
-const stripFunctionNameList = [
-	'alert',
-	'window.alert',
-	'console.log',
-	'window.console.log'
-];
+const globals = new Set(['window', 'globalThis']);
 
+/**
+Collapse `window` and `globalThis`.
+
+@param {import('@babel/core').NodePath} main - Node that may be on a global object.
+
+@returns {import('@babel/core').NodePath} Collapsed node.
+*/
+function collapseGlobal(main) {
+	if (main.isMemberExpression()) {
+		const object = main.get('object');
+		if (object.isIdentifier() && globals.has(object.node.name) && main.has('property')) {
+			return main.get('property');
+		}
+	}
+
+	return main;
+}
+
+/**
+@param {import('@babel/core').NodePath<import('@babel/core').CallExpression>} path
+*/
+function isConsoleNode(path) {
+	const expression = path.get('callee');
+
+	if (!expression.isMemberExpression()) {
+		return;
+	}
+
+	const main = collapseGlobal(expression.get('object'));
+
+	return main.isIdentifier({name: 'console'}) && expression.has('property');
+}
+
+/**
+@param {import('@babel/core').NodePath<import('@babel/core').CallExpression>} path
+*/
+function isAlertNode(path) {
+	const main = collapseGlobal(path.get('callee'));
+
+	return main.isIdentifier({name: 'alert'});
+}
+
+/**
+@returns {import('@babel/core').PluginObj}
+*/
 export default function stripDebugPlugin({types}) {
 	return {
 		visitor: {
@@ -12,16 +52,7 @@ export default function stripDebugPlugin({types}) {
 				path.remove();
 			},
 			CallExpression(path) {
-				const isMatched = stripFunctionNameList.some(functionName => {
-					const calleePath = path.get('callee');
-					if (calleePath.matchesPattern(functionName)) {
-						return !calleePath.node.computed;
-					}
-
-					return calleePath.node.name === functionName;
-				});
-
-				if (isMatched) {
+				if (isConsoleNode(path) || isAlertNode(path)) {
 					path.replaceWith(types.unaryExpression('void', types.numericLiteral(0)));
 				}
 			}
